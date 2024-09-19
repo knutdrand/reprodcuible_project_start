0f744ddb0b170ef78e4a2f57de917855281e7ddb
Knut Rand <knutdrand@gmail.com>
2024-09-19 11:41:00 +0200

    feature: count gc

--- reprodcuible_project_start/reprodcuible_project_start.py
+++ reprodcuible_project_start/reprodcuible_project_start.py
@@ -1,1 +1,5 @@
 """Main module."""
+def count_gc(seq):
+    """Count the number of G's and C's in a sequence."""
+    seq = seq.upper()
+    return seq.count('G') + seq.count('C')
--- tests/test_reprodcuible_project_start.py
+++ tests/test_reprodcuible_project_start.py
@@ -4,18 +4,21 @@
 
 import pytest
 
+from reprodcuible_project_start.reprodcuible_project_start import count_gc
+
 
 @pytest.fixture
-def response():
+def seq():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    return 'acgtcg'
 
-
-def test_content(response):
+def test_content(seq):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert count_gc(seq) == 4
